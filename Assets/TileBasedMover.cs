e389d289ac34a2196a01aecbd67d8376927b105c
benjaminwyss <wyssbenjamin17@gmail.com>
2019-11-22 01:12:59 -0600

    Add mobile controls based on a Unity tutorial. They have yet to be tested since I cannot get Unity Remote to work.

--- Assets/TileBasedMover.cs
+++ Assets/TileBasedMover.cs
@@ -17,14 +17,62 @@ public class TileBasedMover : MonoBehaviour
     private float moveCooldown = 0f;
     private float tileDifficulty = 1f;
 
+    private Vector2 touchOrigin = -Vector2.one;
+
     public Vector3 targetPos;
 
     void Move()
     {
         if (moveCooldown <= 0f)
         {
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
+            float horizontal, vertical;
+
+            #if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+
+            #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+
+            //Check if Input has registered more than zero touches
+            if (Input.touchCount > 0)
+            {
+                //Store the first touch detected.
+                Touch myTouch = Input.touches[0];
+
+                //Check if the phase of that touch equals Began
+                if (myTouch.phase == TouchPhase.Began)
+                {
+                    //If so, set touchOrigin to the position of that touch
+                    touchOrigin = myTouch.position;
+                }
+
+                //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+                else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+                {
+                    //Set touchEnd to equal the position of this touch
+                    Vector2 touchEnd = myTouch.position;
+
+                    //Calculate the difference between the beginning and end of the touch on the x axis.
+                    float x = touchEnd.x - touchOrigin.x;
+
+                    //Calculate the difference between the beginning and end of the touch on the y axis.
+                    float y = touchEnd.y - touchOrigin.y;
+
+                    //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+                    touchOrigin.x = -1;
+
+                    //Check if the difference along the x axis is greater than the difference along the y axis.
+                    if (Mathf.Abs(x) > Mathf.Abs(y))
+                        //If x is greater than zero, set horizontal to 1, otherwise set it to -1
+                        horizontal = x > 0 ? 1 : -1;
+                    else
+                        //If y is greater than zero, set horizontal to 1, otherwise set it to -1
+                        vertical = y > 0 ? 1 : -1;
+                }
+            }
+
+            #endif //End of mobile platform dependendent compilation section started above with #elif
 
             if (horizontal != 0 || vertical != 0)
             {
