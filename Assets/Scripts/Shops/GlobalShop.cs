3498560b086cfb04ccc0e60dbea40bfa2708695f
John Ho <yuho850616@gmail.com>
2019-12-10 09:40:53 -0600

    shop panel

---
+++ Assets/Scripts/Shops/GlobalShop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalShop : MonoBehaviour
+{
+    public static string Item01;
+    public static string Item02;
+    public static string Item03;
+    public static string Item04;
+    public static int ShopNumber;
+
+
+    void Update()
+    {
+        if (ShopNumber == 1)
+        {
+            Item01 = "blue portion";
+            Item02 = "yellow portion";
+            Item03 = "red portion";
+            Item04 = "green portion";
+        }
+        if (ShopNumber == 2)
+        {
+            Item01 = "purple portion";
+            Item02 = "black portion";
+            Item03 = "white portion";
+            Item04 = "orange portion";
+        }
+    }
+}
---
+++ Assets/Scripts/Shops/Shop01Access.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Shop01Access : MonoBehaviour
+{
+    public GameObject ShopInventory;
+    public GameObject Item01Text;
+    public GameObject Item02Text;
+    public GameObject Item03Text;
+    public GameObject Item04Text;
+    public GameObject ItemCompletion;
+    public GameObject CompleteText;
+    void OnTriggerEnter()
+    {
+        ShopInventory.SetActive(true);
+        Screen.lockCursor = false;
+        GlobalShop.ShopNumber = 1;
+        Item01Text.GetComponent<Text>().text = " " + GlobalShop.Item01;
+        Item02Text.GetComponent<Text>().text = " " + GlobalShop.Item02;
+        Item03Text.GetComponent<Text>().text = " " + GlobalShop.Item03;
+        Item04Text.GetComponent<Text>().text = " " + GlobalShop.Item04;
+    }
+    public void Item01()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item01 + "?";
+    }
+    public void Item02()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item02 + "?";
+    }
+    public void Item03()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item03 + "?";
+    }
+    public void Item04()
+    {
+        ItemCompletion.SetActive(true);
+        CompleteText.GetComponent<Text>().text = "Are you sure you want to buy " + GlobalShop.Item04 + "?";
+    }
+    public void CancenlTransaction()
+    {
+        ItemCompletion.SetActive(false);
+    }
+}
+
