06ea2f252af24d059163eed549a65aea3a6c3d5d
Alex King <ktmking611@gmail.com>
2020-01-28 16:14:20 -0600

    added messenger system and added an example scene

---
+++ Assets/Scripts/MessengerExample/Broadcaster.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Broadcaster : MonoBehaviour
+{
+    public void BroadcastButtonClick() {
+        Messenger.Broadcast(GameEvent.BUTTON_CLICKED);
+    }
+}
---
+++ Assets/Scripts/MessengerExample/Listener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Listener : MonoBehaviour
+{
+    private Button _button;
+    private void Awake() {
+        Messenger.AddListener(GameEvent.BUTTON_CLICKED, OnButtonClicked);
+    }
+    private void Start() {
+        _button = GetComponent<Button>();
+    }
+    private void OnDestroy() {
+        Messenger.RemoveListener(GameEvent.BUTTON_CLICKED, OnButtonClicked);
+    }
+    private void OnButtonClicked() {
+        _button.image.color = Color.cyan;
+    }
+}
