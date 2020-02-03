9b81cd1e7ba29e729485ab16c03586684908a6d1
John Ho <yuho850616@gmail.com>
2020-02-03 03:56:03 -0600

    update

--- Assets/SampleButton.cs
+++ Assets/SampleButton.cs
@@ -1,23 +1,37 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
-
 
 public class SampleButton : MonoBehaviour
 {
-    public Button button;
+
+    public Button buttonComponent;
     public Text nameLabel;
-    public Text priceLabel;
     public Image iconImage;
-    // Start is called before the first frame update
+    public Text priceText;
+
+
+    private Itemtemp item;
+    private ShopScrollList scrollList;
+
+    // Use this for initialization
     void Start()
     {
-        
+        buttonComponent.onClick.AddListener(HandleClick);
     }
 
-    public void Setup()
+    public void Setup(Itemtemp currentItem, ShopScrollList currentScrollList)
     {
+        item = currentItem;
+        nameLabel.text = item.itemName;
+        iconImage.sprite = item.icon;
+        priceText.text = item.price.ToString();
+        scrollList = currentScrollList;
 
     }
 
+    public void HandleClick()
+    {
+        scrollList.TryTransferItemToOtherShop(item);
+    }
 }
--- Assets/ShopScrollList.cs
+++ Assets/ShopScrollList.cs
@@ -1,38 +1,95 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 [System.Serializable]
 public class Itemtemp
 {
     public string itemName;
     public Sprite icon;
-    public float price = 1f;
+    public float price = 1;
 }
-
-
 
 public class ShopScrollList : MonoBehaviour
 {
+
     public List<Itemtemp> itemList;
     public Transform contentPanel;
     public ShopScrollList otherShop;
     public Text myGoldDisplay;
     public SimpleObjectPool buttonObjectPool;
+
     public float gold = 20f;
 
-    private void Start()
+
+    // Use this for initialization
+    void Start()
     {
-        
+        RefreshDisplay();
+    }
+
+    void RefreshDisplay()
+    {
+        myGoldDisplay.text = "Gold: " + gold.ToString();
+        RemoveButtons();
+        AddButtons();
+    }
+
+    private void RemoveButtons()
+    {
+        while (contentPanel.childCount > 0)
+        {
+            GameObject toRemove = transform.GetChild(0).gameObject;
+            buttonObjectPool.ReturnObject(toRemove);
+        }
     }
 
     private void AddButtons()
     {
-        for (int i = 0; i<itemList.Count; i++)
+        for (int i = 0; i < itemList.Count; i++)
         {
             Itemtemp item = itemList[i];
             GameObject newButton = buttonObjectPool.GetObject();
+            newButton.transform.SetParent(contentPanel);
+
+            SampleButton sampleButton = newButton.GetComponent<SampleButton>();
+            sampleButton.Setup(item, this);
+        }
+    }
+
+    public void TryTransferItemToOtherShop(Itemtemp item)
+    {
+        if (otherShop.gold >= item.price)
+        {
+            gold += item.price;
+            otherShop.gold -= item.price;
+
+            AddItem(item, otherShop);
+            RemoveItem(item, this);
+
+            RefreshDisplay();
+            otherShop.RefreshDisplay();
+            Debug.Log("enough gold");
+
+        }
+        Debug.Log("attempted");
+    }
+
+    void AddItem(Itemtemp itemToAdd, ShopScrollList shopList)
+    {
+        shopList.itemList.Add(itemToAdd);
+    }
+
+    private void RemoveItem(Itemtemp itemToRemove, ShopScrollList shopList)
+    {
+        for (int i = shopList.itemList.Count - 1; i >= 0; i--)
+        {
+            if (shopList.itemList[i] == itemToRemove)
+            {
+                shopList.itemList.RemoveAt(i);
+            }
         }
     }
 }
+
