50899551ea9a2ead167d0e770e9c8b1b0a517b59
John Ho <yuho850616@gmail.com>
2020-02-06 18:50:09 -0600

    fake vendor

---
+++ Assets/Scripts/Vendor/Vendor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vendor : MonoBehaviour, IInteractable
+{
+  public void Interact()
+  {
+    Debug.Log("interact");
+  }
+
+  public void StopInteract()
+  {
+    Debug.Log("Stop interact");
+  }
+}
