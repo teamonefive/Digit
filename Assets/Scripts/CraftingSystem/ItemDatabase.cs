c8b949e59f74c271db2a97389213c20402ebb83a
Alex King <ktmking611@gmail.com>
2020-02-16 19:37:00 -0600

    added crafting system to mapgen. Still need to update artwork and remove crafting components from the players inventory when a new item is crafted

---
+++ Assets/Scripts/CraftingSystem/ItemDatabase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour {
+
+    public List<Item> m_items = new List<Item>();
+
+    private void Awake() {
+        BuildItemDatabase();
+    }
+
+
+    //Looks through the database and returns an item if it finds one that matches the input id else returns null
+    public Item GetItem(int id) {
+        return m_items.Find(item => item.m_id == id);
+    }
+
+
+    void BuildItemDatabase() {
+        m_items = new List<Item>() {
+            new Item(0,"NULL",Item.ItemType.EMPTY,Item.Rarity.COMMON , "NULL",
+            new Dictionary<string, int>{
+                { "NULL", 0 }
+            }),
+            new Item(1,"Diamond Pickaxe",Item.ItemType.POWERUP, Item.Rarity.EPIC, "Congratulations, you have reached the pinacle of antiquated mining technology!",
+            new Dictionary<string, int>{
+                { "BLOCKDAMAGE", 100 },
+                { "SPEED", 2 },
+                { "VALUE", 1000 }
+            }),
+            new Item(2,"Iron Ore",Item.ItemType.COMPONENT, Item.Rarity.COMMON, "a chunk of raw iron",
+            new Dictionary<string, int>{
+                { "VALUE", 10 }
+            }),
+            new Item(3,"Topaz",Item.ItemType.COMPONENT, Item.Rarity.COMMON, "a chunk of topaz",
+            new Dictionary<string, int>{
+                { "VALUE", 30 }
+            }),
+            new Item(4,"Sapphire",Item.ItemType.COMPONENT, Item.Rarity.RARE, "a chunk of sapphire",
+            new Dictionary<string, int>{
+                { "VALUE", 50 }
+            }),
+            new Item(5,"Ruby",Item.ItemType.COMPONENT, Item.Rarity.RARE, "a chunk of ruby",
+            new Dictionary<string, int>{
+                { "VALUE", 100 }
+            }),
+            new Item(6,"Diamond",Item.ItemType.COMPONENT, Item.Rarity.RARE, "a chunk of diamond",
+            new Dictionary<string, int>{
+                { "VALUE", 400 }
+            }),
+            new Item(10,"Silver Pickaxe",Item.ItemType.COMPONENT, Item.Rarity.RARE, "a pickaxe made of steel",
+            new Dictionary<string, int>{
+                { "BLOCKDAMAGE", 50 },
+                { "SPEED", 2 },
+                { "VALUE", 100 }
+            })
+        };
+    }
+}
---
+++ Assets/Scripts/CraftingSystem/ItemOverview.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemOverview : MonoBehaviour{
+    public ItemDatabase itemDatabase;
+    public Item m_item;
+    public Text m_name;
+    public Text m_description;
+    public Text m_stats;
+    public Image m_image;
+
+    private void Awake() {
+        itemDatabase = FindObjectOfType<ItemDatabase>();
+    }
+    private void Start() {
+        m_item = itemDatabase.GetItem(0);
+        m_name.text = m_item.m_name;
+        m_description.text = m_item.m_description;
+        m_image.sprite = Resources.Load<Sprite>("Items/" + m_item.m_name);
+        m_stats.text = "";
+        switch ( m_item.m_rarity ) {
+            case Item.Rarity.COMMON:
+                m_name.color = Color.white;
+                break;
+            case Item.Rarity.RARE:
+                m_name.color = Color.blue;
+                break;
+            case Item.Rarity.EPIC:
+                m_name.color = Color.magenta;
+                break;
+            case Item.Rarity.LEGENDARY:
+                m_name.color = Color.yellow;
+                break;
+        }
+    }
+
+    public void updateItemOverview(Item item) {
+        Debug.Log("updateItemOverview called with item " + item.m_id);
+        m_item = item;
+        m_name.text = m_item.m_name;
+        m_description.text = m_item.m_description;
+        m_image.sprite = Resources.Load<Sprite>("Items/" + m_item.m_name);
+        m_stats.text = formatStats(item);
+        switch ( m_item.m_rarity ) {
+            case Item.Rarity.COMMON:
+                m_name.color = Color.white;
+                break;
+            case Item.Rarity.RARE:
+                m_name.color = Color.blue;
+                break;
+            case Item.Rarity.EPIC:
+                m_name.color = Color.magenta;
+                break;
+            case Item.Rarity.LEGENDARY:
+                m_name.color = Color.yellow;
+                break;
+        }
+    }
+
+    private string formatStats(Item item) {
+        string val = "";
+        foreach(KeyValuePair<string, int> entry in item.m_stats ) {
+            val += "<b>" + entry.Key + "</b>: " + entry.Value + "\n";
+        }
+        return val;
+    }
+}
