6d50ff97fe26de0198c8c6267198694be47abea4
John Ho <yuho850616@gmail.com>
2020-02-22 03:46:39 -0600

    tab view

--- Assets/Scripts/PanelOpener.cs
+++ Assets/Scripts/PanelOpener.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PanelOpener : MonoBehaviour
 {
     public GameObject Panel;
+    public GameObject Panel1;
+    public GameObject Panel2;
+    public GameObject Panel3;
 
     public void OpenPanel()
     {
@@ -12,6 +15,33 @@ public class PanelOpener : MonoBehaviour
         {
             bool isActive = Panel.activeSelf;
             Panel.SetActive(!isActive);
+        }
+    }
+
+    public void OpenPanel1()
+    {
+        if (Panel1 != null)
+        {
+            bool isActive = Panel1.activeSelf;
+            Panel1.SetActive(!isActive);
+        }
+    }
+
+    public void OpenPanel2()
+    {
+        if (Panel2 != null)
+        {
+            bool isActive = Panel2.activeSelf;
+            Panel2.SetActive(!isActive);
+        }
+    }
+
+    public void OpenPanel3()
+    {
+        if (Panel3 != null)
+        {
+            bool isActive = Panel3.activeSelf;
+            Panel3.SetActive(!isActive);
         }
     }
 }
