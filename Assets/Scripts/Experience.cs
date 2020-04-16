924396f9a1a4a79907d660f56a4c050b07d4f38e
jeffkissick <jkissick22@hotmail.com>
2020-04-15 19:58:32 -0500

    Level up system now allows users to customize their stats.  You get the same amount of stat points as your level and you can only take away points from stats you have added to at that current level up. Next ill add a check to make sure those are the stats they want, currently the menu disappears after they add the last stat.  I may also make a cap on stats, say 2 x current level fas the highest they can level a single stat at a time.

--- Assets/Scripts/Experience.cs
+++ Assets/Scripts/Experience.cs
@@ -13,14 +13,33 @@ public class Experience : MonoBehaviour
     public Text currLevel;
     public Text strengthMod;
     public Text agilityMod;
+    public Text enduranceMod;
     public Text perceptionMod;
     public Text luckMod;
     public GameObject ui;
     public Button menuButton;
     public Stats stat;
     public Text myGoldDisplay;
+    public GameObject plusPanel;
+    public GameObject minusPanel;
+    public GameObject Dwarf;
+    public Button strP;
+    public Button agP;
+    public Button endP;
+    public Button perP;
+    public Button luckP;
+    public Button strM;
+    public Button agM;
+    public Button endM;
+    public Button perM;
+    public Button luckM;
+    private int statPoints = 0;
+    private int strUp = 0;
+    private int agUp = 0;
+    private int endUp = 0;
+    private int perUp = 0;
+    private int lucUp = 0;
     public int MyGold { get; set; }
-
 
     private bool isOpen = true;
 
@@ -56,20 +75,65 @@ public class Experience : MonoBehaviour
         luckMod.text = "Luck: 0";
         Button butn = menuButton.GetComponent<Button>();
         butn.onClick.AddListener(TaskOnClick);
+        Button strp = strP.GetComponent<Button>();
+        strp.onClick.AddListener(strengthUp);
+        Button agp = agP.GetComponent<Button>();
+        agp.onClick.AddListener(agilityUp);
+        Button endp = endP.GetComponent<Button>();
+        endp.onClick.AddListener(enduranceUp);
+        Button perp = perP.GetComponent<Button>();
+        perp.onClick.AddListener(perceptionUp);
+        Button lucp = luckP.GetComponent<Button>();
+        lucp.onClick.AddListener(luckUp);
+
+        Button strm = strM.GetComponent<Button>();
+        strm.onClick.AddListener(strengthDown);
+        Button agm = agM.GetComponent<Button>();
+        agm.onClick.AddListener(agilityDown);
+        Button endm = endM.GetComponent<Button>();
+        endm.onClick.AddListener(enduranceDown);
+        Button perm = perM.GetComponent<Button>();
+        perm.onClick.AddListener(perceptionDown);
+        Button lucm = luckM.GetComponent<Button>();
+        lucm.onClick.AddListener(luckDown);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (statPoints > 0)
+        {
+            StartCoroutine(showMinusStats());
+        }
         if (tile.isDestroyed == true) // bool is set in TileBasedMover after a block is destroyed, line 360
         {
             GainExp(1);
-            print("XP Gained"); // prints to the console for debugging
+           // print("XP Gained"); // prints to the console for debugging
             levelUpBar.value = stat.vCurrExp;
         }
         tile.isDestroyed = false; // reset the bool here NOT in TileBasedMover
         enduranceBar.value = enduranceBar.maxValue - stat.vFatigue;
 
+        if (strUp > 0)
+        {
+            StartCoroutine(showStr());
+        }
+        if (agUp > 0)
+        {
+            StartCoroutine(showAg());
+        }
+        if (endUp > 0)
+        {
+            StartCoroutine(showEnd());
+        }
+        if (perUp > 0)
+        {
+            StartCoroutine(showPer());
+        }
+        if(lucUp > 0)
+        {
+            StartCoroutine(showLuck());
+        }
         if(Input.GetKeyDown(KeyCode.B))
         {
             InventoryScript.MyInstance.OpenClose();
@@ -93,11 +157,11 @@ public class Experience : MonoBehaviour
         float t = Mathf.Pow(stat.vExpMod, stat.vLevel);
         stat.vExpLeft = (int)Mathf.Floor(stat.vExpBase * t);
         levelUpBar.maxValue = stat.vExpLeft;
-        strengthUp();
-        agilityUp();
-        perceptionUp();
-        luckUp();
-        if(trig4 == true)
+        plusPanel.SetActive(true);
+        Dwarf.GetComponent<TileBasedMover>().enabled = false;
+        StartCoroutine(allocatingStats());
+        
+        if (trig4 == true)
         {
             trig4 = false;
             FindObjectOfType<DialogueTrigger>().TriggerLvlUpDialogue();
@@ -108,23 +172,76 @@ public class Experience : MonoBehaviour
         stat.vStrength++;
         strengthMod.text = "Strength: " + stat.vStrength.ToString();
         stat.strengthMultiplier *= 0.95f;
+        statPoints++;
+        strUp++;
     }
     void agilityUp()
     {
         stat.vAgility++;
         agilityMod.text = "Agility: " + stat.vAgility.ToString();
         stat.climbingDifficulty *= 0.95f;
+        statPoints++;
+        agUp++;
         //tile.moveSpeed *= 1.5f;
+    }
+    void enduranceUp()
+    {
+        stat.vEndurance++;
+        enduranceMod.text = "Endurance: " + stat.vEndurance.ToString();
+        statPoints++;
+        endUp++;
     }
     void perceptionUp()
     {
         stat.vPerception++;
         perceptionMod.text = "Perception: " + stat.vPerception.ToString();
+        statPoints++;
+        perUp++;
     }
     void luckUp()
     {
         stat.vLuck++;
         luckMod.text = "Luck: " + stat.vLuck.ToString();
+        statPoints++;
+        lucUp++;
+    }
+    void strengthDown()
+    {
+        stat.vStrength--;
+        strengthMod.text = "Strength: " + stat.vStrength.ToString();
+        stat.strengthMultiplier *= 0.95f;
+        statPoints--;
+        strUp--;
+    }
+    void agilityDown()
+    {
+        stat.vAgility--;
+        agilityMod.text = "Agility: " + stat.vAgility.ToString();
+        stat.climbingDifficulty *= 0.95f;
+        statPoints--;
+        agUp--;
+        //tile.moveSpeed *= 1.5f;
+    }
+    void enduranceDown()
+    {
+        stat.vEndurance--;
+        enduranceMod.text = "Endurance: " + stat.vEndurance.ToString();
+        statPoints--;
+        endUp--;
+    }
+    void perceptionDown()
+    {
+        stat.vPerception--;
+        perceptionMod.text = "Perception: " + stat.vPerception.ToString();
+        statPoints--;
+        perUp--;
+    }
+    void luckDown()
+    {
+        stat.vLuck--;
+        luckMod.text = "Luck: " + stat.vLuck.ToString();
+        statPoints--;
+        lucUp--;
     }
     void TaskOnClick() 
     {
@@ -151,5 +268,53 @@ public class Experience : MonoBehaviour
             clickable.MyIcon.color = new Color(0, 0, 0, 0);
             clickable.MyStackText.color = new Color(0, 0, 0, 0);
         }
+    }
+    IEnumerator allocatingStats()
+    {
+        yield return new WaitUntil(() => statPoints == stat.vLevel);
+        plusPanel.SetActive(false);
+        Dwarf.GetComponent<TileBasedMover>().enabled = true;
+        statPoints = 0;
+        strUp = 0;
+        agUp = 0;
+        endUp = 0;
+        perUp = 0;
+        lucUp = 0;
+    }
+    IEnumerator showMinusStats()
+    {
+        minusPanel.SetActive(true);
+        yield return new WaitUntil(() => statPoints == 0);
+        minusPanel.SetActive(false);
+    }
+    IEnumerator showStr()
+    {
+        strM.GetComponent<Image>().enabled = true;
+        yield return new WaitUntil(() => strUp == 0);
+        strM.GetComponent<Image>().enabled = false;
+    }
+    IEnumerator showAg()
+    {
+        agM.GetComponent<Image>().enabled = true;
+        yield return new WaitUntil(() => agUp == 0);
+        agM.GetComponent<Image>().enabled = false;
+    }
+    IEnumerator showEnd()
+    {
+        endM.GetComponent<Image>().enabled = true;
+        yield return new WaitUntil(() => endUp == 0);
+        endM.GetComponent<Image>().enabled = false;
+    }
+    IEnumerator showPer()
+    {
+        perM.GetComponent<Image>().enabled = true;
+        yield return new WaitUntil(() => perUp == 0);
+        perM.GetComponent<Image>().enabled = false;
+    }
+    IEnumerator showLuck()
+    {
+        luckM.GetComponent<Image>().enabled = true;
+        yield return new WaitUntil(() => lucUp == 0);
+        luckM.GetComponent<Image>().enabled = false;
     }
 }
